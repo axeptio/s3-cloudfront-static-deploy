06f2f8e5b1274d27428eed675cd12d62d63be86a
arknoid <omalige@gmail.com>
2018-12-21 23:31:05 +0100

    added : rollbackFile, listFileVersions,enableVersioning methods to API

--- test/test.js
+++ test/test.js
@@ -2,7 +2,12 @@
  * Require
  */
 
-const { publishJson } = require('../');
+const {
+  publishFile,
+  listFileVersions,
+  enableVersioning,
+  rollbackFile
+} = require('../');
 const randomWords = require('random-words');
 
 /*
@@ -18,17 +23,54 @@ const { myBucket, filename, distributionId } = require('./config');
 
 // Generate Json with random words
 const content = JSON.stringify({
-  name: 'testing with a random JSON',
+  name: 'random JSON',
   words: randomWords(30)
 });
 
 
+// Methods to test
+const methods = {
+  publishFile: true,
+  enableVersioning: true,
+  listFileVersions: true,
+  rollbackFile: true
+};
+
 (async () => {
   try {
-    let publishResult = await publishJson(myBucket, filename, distributionId, content, event => {
-      console.log(event);
-    });
-    console.dir(publishResult, { depth: 5 });
+    if (methods.publishFile) {
+      const publishResult = await publishFile(
+        myBucket,
+        filename,
+        distributionId,
+        content,
+        event => {
+          console.log(event);
+        }
+      );
+      console.dir(publishResult, { depth: 5 });
+    }
+
+    if (methods.enableVersioning) {
+      const bucketVersioningResult = await enableVersioning(myBucket, true, event => {
+        console.log(event);
+      });
+      console.log(bucketVersioningResult);
+    }
+
+    if (methods.listFileVersions) {
+      const versionsResult = await listFileVersions(myBucket, filename, event => {
+        console.log(event);
+      });
+      console.log(versionsResult);
+    }
+
+    if (methods.rollbackFile) {
+      const rollbackResult = await rollbackFile(myBucket, filename, event => {
+        console.log(event);
+      });
+      console.log(rollbackResult);
+    }
   } catch (err) {
     console.log(err.message);
   }
